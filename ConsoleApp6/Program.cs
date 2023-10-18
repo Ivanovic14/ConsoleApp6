1e8d978c87894801dd1ce7456d1f20ff58db4d25
ival06001 <ival06001@E-PW024NVZ>
2023-10-18 12:52:53 +0200

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Uppgift_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int start = 10; start <= 30; start += 1)
+            {
+                Console.WriteLine(start);
+            }
+            for (int i = 200; 180 <= i; i--)
+            {
+                Console.WriteLine(i);
+            }
+            for (int hej = 1000; hej <= 1400; hej += 50)
+            {
+                Console.WriteLine(hej);
+            }
+        }
+    }
+}
